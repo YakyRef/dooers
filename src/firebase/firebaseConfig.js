10c8b6007062c8c3df540283ea9d53c1de9b8c20
yakyRef <yaky.ref@gmail.com>
2021-04-01 21:07:04 +0300

    dsf

--- src/firebase/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA4umQQcNQixOgoQF7ZwySxLcNB87Gier8",
-  authDomain: "ytpl-dev.firebaseapp.com",
-  projectId: "ytpl-dev",
-  storageBucket: "ytpl-dev.appspot.com",
-  messagingSenderId: "676656623241",
-  appId: "1:676656623241:web:1b30df6cd07a0b1ced2058",
-};
-export default firebaseConfig;
