c8de3b34ce43f7890f3991b383ae3c5ef963c963
yakyRef <yaky.ref@gmail.com>
2021-04-01 18:42:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const USER_DETAILS = {
+  user: "yaky",
+  email: "asd@asd.sd",
+};
 
 function App() {
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log("uuuu");
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h5>Yo Roey! x</h5>
       </header>
     </div>
   );
